dc23217bb8de2ad19c286e934bba7cd5bbbee80e
Janick Braun <nasebuch@gmail.com>
2026-04-24 13:10:49 +0200

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import CookieBanner from "@/components/CookieBanner";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({
@@ -83,6 +84,7 @@ export default function RootLayout({
         {children}
         {/* <CookieBanner /> */}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
